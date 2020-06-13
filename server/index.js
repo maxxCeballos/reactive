8ef500610c7834ce7f9182e97e2a85d7cccb7e42
german <gkiessling_95@hotmail.com>
2020-06-13 15:48:58 -0300

    mensajes conexion server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 'use strict'
 
-const app = require('express')();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const port = 3000;
+const app = require('express')(); // Import del modulo express.js
+const server = require('http').createServer(app); // Import del servidor http
+const io = require('socket.io')(server);  //Import del modulo socket.io
+//const port = 3000;
+const port = process.env.PORT || 3000;
 // salas de chat
 const python = io.of('/python');
 const javascript = io.of('/javascript');
@@ -34,6 +35,35 @@ app.get('/', function(req, res) {
 // });
 // })
 
+// Cuando hay una conexion...
+io.on("connection", function (socket) {
+
+  socket.on("name", function (name) {
+    nombres.set(name, socket.id);
+    clientes.set(socket.id, name);
+    console.log("Se conecto " + name);
+  });
+
+  // Al recibir un mensaje, hacer...
+  socket.on("messageTo", function (msg) {
+    // Enviar mensaje a todos los nombres menos al emisor
+    console.log(msg);
+    var destinatario = nombres.get(msg.to);
+    var emisor = clientes.get(socket.id);
+    io.to(destinatario).emit('message', { "message": msg.message, "from": emisor });
+  });
+
+  socket.on("disconnect", function () {
+
+    var userName=clientes.get(socket.id);
+    console.log(userName+' Cerro la sesion');
+
+    nombres.delete(clientes.get(socket.id))
+    clientes.delete(socket.id);
+  });
+
+
+});
 
 // python.on('connection', function(socket){
 
